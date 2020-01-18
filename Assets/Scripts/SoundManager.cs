ef78319d65f22699156626c3b0cb1875b6d47aaf
c0d3d1 <55641025+c0d3d1@users.noreply.github.com>
2020-01-18 15:52:32 -0700

    Revert "Revert "Merge branch 'master' of https://github.com/CedarBishop/WowieJam""
    This reverts commit e1a4fe6afe5545102620108c82be04e4a35c3d68.

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -20,16 +20,24 @@ public class SoundManager : MonoBehaviour
         switch (clip)
         {
             case "Hit":
+                audioSrc.volume = 1f;
                 audioSrc.PlayOneShot(Hit);
+                audioSrc.volume = 1f;
                 break;
             case "Shoot":
+                audioSrc.volume = 1f;
                 audioSrc.PlayOneShot(Shoot);
+                audioSrc.volume = 1f;
                 break;
             case "Move":
+                audioSrc.volume = 1f;
                 audioSrc.PlayOneShot(Move);
+                audioSrc.volume = 1f;
                 break;
             case "Transistion":
+                audioSrc.volume = 0.5f;
                 audioSrc.PlayOneShot(Transistion);
+                audioSrc.volume = 1f;
                 break;
         }
     }
