c69da5281cecaef9856661fd7328f5026b1a170a
c0d3d1 <55641025+c0d3d1@users.noreply.github.com>
2020-01-17 18:43:59 -0700

    First push

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private const int GRID_SIZE = 1;
+    private const int MOVE_TIME = 1;
+    public float Speed = 2;
+    public LayerMask wall;
+    private Vector2 target;
+    private bool canMove = false;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W) )
+        {
+            if (!Physics.Raycast(transform.position, Vector2.up, GRID_SIZE))
+            {
+                target = new Vector2(transform.position.x, transform.position.y + 1);
+                canMove = true;
+            }
+
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            if (!Physics.Raycast(transform.position, Vector2.down, GRID_SIZE))
+            {
+                target = new Vector2(transform.position.x, transform.position.y - 1);
+                canMove = true;
+            }
+
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            if (!Physics.Raycast(transform.position, Vector2.left, GRID_SIZE))
+            {
+                target = new Vector2(transform.position.x - 1, transform.position.y);
+                canMove = true;
+            }
+
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            if (!Physics.Raycast(transform.position, Vector2.right, GRID_SIZE))
+            {
+                target = new Vector2(transform.position.x + 1, transform.position.y);
+                canMove = true;
+            }
+
+        }
+        if (canMove) 
+        {
+            //Vector2.MoveTowards(transform.position, target, Speed);
+            transform.position = target;
+        }
+    }
+}
