a3d13c2c4a50ff40641fd120ef2833a767e69420
Cedar Bishop <47874454+CedarBishop@users.noreply.github.com>
2020-01-19 09:45:51 +1100

    Fixed melee

--- Assets/Scripts/Melee.cs
+++ Assets/Scripts/Melee.cs
@@ -5,18 +5,31 @@ using UnityEngine;
 public class Melee : Enemy
 {
     private Player player;
+    public Vector2 size = new Vector2(3, 3);
 
     public int attackCount = 4;
 
-    private void Start()
-    {
-        player = FindObjectOfType<Player>();
-    }
     public void Attack()
     {
-        if (attackCount > 0 && gameObject.GetComponentInChildren<Zone>().touchingPlayer == true)
+        if (attackCount > 0)
         {
-            player.Die();
+            attackCount--;
+            RaycastHit2D[] hit = Physics2D.BoxCastAll(transform.position, size, 0, Vector2.up);
+            if (hit == null)
+            {
+                return;
+            }
+
+            for (int i = 0; i < hit.Length; i++)
+            {
+                if (hit[i].collider.gameObject != gameObject)
+                {
+                    if (hit[i].collider.GetComponent<Character>())
+                    {
+                        hit[i].collider.GetComponent<Character>().Die();
+                    }
+                }
+            }
         }
     }
 }
