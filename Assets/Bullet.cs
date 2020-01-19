058a1d450ab19990518aac473a9218832c67d6d8
c0d3d1 <55641025+c0d3d1@users.noreply.github.com>
2020-01-19 13:55:31 -0700

    Artist wanted bullets to move for art purposes.

---
+++ Assets/Bullet.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public Vector2 direction;
+    public GameObject parent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, 10);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(direction * Time.deltaTime * 10);
+    }
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject != parent)
+        {
+
+       
+        if (col.gameObject.GetComponent<Player>() != null)
+        {
+            col.gameObject.GetComponent<Player>().Die();
+        }
+        else if (col.gameObject.GetComponent<Shooter>() != null)
+        {
+            col.gameObject.GetComponent<Shooter>().Die();
+        }
+        else if (col.gameObject.GetComponent<Melee>() != null)
+        {
+            col.gameObject.GetComponent<Melee>().Die();
+        }
+        }
+    }
+}
